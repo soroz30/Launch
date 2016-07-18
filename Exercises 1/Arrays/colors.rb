28a14ec6fc999310cf84cc142a651654798bca86
Marek Koseski <soroz30@yahoo.pl>
2016-07-18 14:36:37 +0200

    Arrays, Hashes

---
+++ Exercises 1/Arrays/colors.rb
@@ -0,0 +1,5 @@
+colors = ['red', 'yellow', 'purple', 'green']
+
+colors.each do |ele|
+  puts "I'm the color " + ele + "!" 
+end
---
+++ Exercises 1/Arrays/doubled.rb
@@ -0,0 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+
+doubled_numbers = numbers.map {|ele| ele * 2}
+
+p doubled_numbers
